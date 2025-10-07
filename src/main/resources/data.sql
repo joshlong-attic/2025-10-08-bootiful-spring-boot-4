53f132585d87189c703cc284fe10f906d937bbbb
Josh Long <54473+joshlong@users.noreply.github.com>
2025-10-07 16:03:02 +0200

    db goodness

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into dog(name, description) values ('Prancer', 'A neurotic, man-hating, animal-hating, children hating dog that looks like a gremlin');
+insert into dog(name, description) values ('Molly', 'A sweet, loving, and affectionate dog');
+insert into dog(name, description) values ('Howard', 'Not a duck! a good dog.');
