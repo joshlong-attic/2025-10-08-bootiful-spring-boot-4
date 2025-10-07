d1c927e096aba53f4fc7c9b9f0b9cc5daa9a2ce0
Josh Long <54473+joshlong@users.noreply.github.com>
2025-10-07 14:01:44 +0200

    it works

--- src/main/java/com/example/cats/CatsApplication.java
+++ src/main/java/com/example/cats/CatsApplication.java
@@ -2,6 +2,9 @@ package com.example.cats;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.beans.factory.BeanRegistrar;
 import org.springframework.beans.factory.BeanRegistry;
 import org.springframework.boot.ApplicationArguments;
@@ -9,11 +12,15 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.core.env.Environment;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.jdbc.core.dialect.JdbcPostgresDialect;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 import org.springframework.resilience.annotation.Retryable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,13 +35,35 @@ import java.util.concurrent.atomic.AtomicInteger;
  * todo JmsClient
  * todo Resilience Methods
  */
-
 @SpringBootApplication
+@EnableResilientMethods
+@ImportRuntimeHints(CatsApplication.MyHints.class)
 @ImportHttpServices(CatFacts.class)
-@Import(DogBeanRegistrar.class)
+@Import(CatsApplication.MyBeanRegistrar.class)
 public class CatsApplication {
 
-    static void main(String[] args) {
+    static class MyBeanRegistrar implements BeanRegistrar {
+
+        @Override
+        public void register(@NonNull BeanRegistry registry, @NonNull Environment env) {
+            registry.registerBean(Runner.class);
+            registry.registerBean(RiskyClient.class);
+            registry.registerBean(JdbcPostgresDialect.class, c -> c
+                    .supplier(_ -> JdbcPostgresDialect.INSTANCE));
+        }
+    }
+
+    /* bleargh. */
+    static class MyHints implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
+            hints.reflection().registerType(tools.jackson.databind.jsontype.NamedType.class);
+            hints.resources().registerPattern("static/resources/**");
+        }
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(CatsApplication.class, args);
     }
 }
@@ -99,29 +128,18 @@ record Runner(DogRepository repository, RiskyClient riskyClient, CatFacts catFac
 
     @Override
     public void run(@NonNull ApplicationArguments args) throws Exception {
-
-        this.riskyClient.connect();
-
         this.repository.findAll().forEach(IO::println);
-
         this.catFacts.facts().facts().forEach(IO::println);
+        this.riskyClient.connect();
     }
 }
 
-
-class DogBeanRegistrar implements BeanRegistrar {
-
-    @Override
-    public void register(@NonNull BeanRegistry registry, @NonNull Environment env) {
-        registry.registerBean(Runner.class);
-        registry.registerBean(RiskyClient.class);
-        registry.registerBean(JdbcPostgresDialect.class, c -> c
-                .supplier(_ -> JdbcPostgresDialect.INSTANCE));
-    }
-}
 
 interface DogRepository
         extends ListCrudRepository<@NonNull Dog, @NonNull Integer> {
+
+    @Query("SELECT * FROM DOG WHERE NAME = :name")
+    Collection <Dog> findByName(@NonNull String name);
 }
 
 record Dog(@Id int id, String description, String name) {
--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-delete from dog;
-insert into dog (id, name,  description) values (1,  'John', 'A good boy');
-insert into dog (id, name,  description) values (45,  'Prancer',
- 'a man hating, children hating neurotic dog that looks like a gremlin');
